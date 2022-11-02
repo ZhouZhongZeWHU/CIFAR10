b1e66a49bc1d15227710814a000f096b15212552
ZhouZhongZeWHU <67944625+ZhouZhongZeWHU@users.noreply.github.com>
2022-11-02 13:10:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -86,24 +86,16 @@ if __name__ == '__main__':
     optimizer = optim.SGD(net.parameters(), lr=learnL, momentum=mom)
 
     for epoch in range(epochNum):
-
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
-
-            # zero the parameter gradients
             optimizer.zero_grad()
-
-            # forward + backward + optimize
             outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-
-            # print statistics
             running_loss += loss.item()
-            if i % 200 == 199:  # print every 2000 mini-batches
+            if i % 200 == 199:  
                 print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
                 running_loss = 0.0
 
@@ -121,9 +113,7 @@ if __name__ == '__main__':
     with torch.no_grad():
         for data in testloader:
             images, labels = data
-            # calculate outputs by running images through the network
             outputs = net(images)
-            # the class with the highest energy is what we choose as prediction
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
